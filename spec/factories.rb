47a8a63d9707dae6ee000f873f3f6e8536b3762c
root <root@debian>
2022-10-01 12:48:00 +0200

    loop integration test

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     full_name { Faker::Name.unique.name }
     name { Faker::Name.unique.first_name }   # ensuring we get unique values
-    email { "#{Faker::Name.unique.first_name}@#{Faker::Internet.domain_name}.cz" }
+    email { "#{Faker::Name.unique.first_name}@#{Faker::Internet.domain_name}" }
     password { Faker::Internet.password }
     password_confirmation { password }
   end
--- spec/requests/delete_user_spec.rb
+++ spec/requests/delete_user_spec.rb
@@ -14,7 +14,28 @@ RSpec.describe 'Users', type: :request do
       end
     end
 
-    context "without admin role" do   # TOHLE PROJÍT NEMÁ, USER NENÍ ADMIN, NEMŮŽE SE SÁM SMAZAT
+    context "with admin role" do
+      before do
+        user.role = "ADMIN"
+        user.save!                                                  # VERY IMPORTANT STEP!
+        FactoryBot.create_list(:user, 10)
+        post '/api/v1/auth/login', params:
+          {
+            email: user.email,
+            password: user.password
+          }
+        @token = json['token']
+      end
+
+      (2..11).each do |id| # the ADMIN has an id of 1, we created 10 more users so 2..11
+        it "should delete user id #{id}" do
+          delete "/api/v1/users/#{id}", headers: { Authorization: @token }
+          expect(response).to have_http_status(:success)
+        end
+      end
+    end
+
+    context "without admin role" do
       before do
         post '/api/v1/auth/login', params:
           {
